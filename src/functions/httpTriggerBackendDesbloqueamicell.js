5dd725c63009467f82738554dade8a24d2d3dce5
robertoclunarc <robertoclunarg@gmail.com>
2024-02-12 09:01:00 -0400

    add function post Checkout

--- src/functions/httpTriggerBackendDesbloqueamicell.js
+++ src/functions/httpTriggerBackendDesbloqueamicell.js
@@ -28,18 +28,25 @@ app.http('httpTriggerBackendDesbloqueamicell', {
             const tab = request.query.get('tab');
             const endPoint = request.query.get('endpoint');
             switch (request.method) {
-                case "GET":                                       
-                    const responseGet = await getItem(connection, tab,  id);
-                    context.res = {
-                        body: JSON.stringify(responseGet),
-                    };
+                case "GET":
+                    if (endPoint && endPoint == 'checkout'){
+                        const responseCheckout = await validateCheckout(connection, id);
+                        context.res = {
+                            body: JSON.stringify(responseCheckout)
+                        }
+                    }else{
+                        const responseGet = await getItem(connection, tab,  id);
+                        context.res = {
+                            body: JSON.stringify(responseGet),
+                        };
+                    }
                     break;
                 
                 case "POST":
                     const requestDataPost = JSON.parse(await request.text());
                     if (requestDataPost) {
                         if (endPoint && endPoint == 'create-checkout-session'){
-                            const createSession = await createCheckoutSession(requestDataPost);
+                            const createSession = await createCheckoutSession(connection, requestDataPost);
                             console.log(createSession);
                             context.res = {                            
                                 body: JSON.stringify(createSession),
@@ -98,23 +105,158 @@ app.http('httpTriggerBackendDesbloqueamicell', {
     }
 });
 
-async function createCheckoutSession(postRequest){
+async function validateCheckout(connection, sessionId){
+    const apiDrSimCreateOrden =process.env.apiDrSimCreateOrden; 
+    const reqGetOrdenDB = await getItem(connection, 'ordenes',  sessionId)
+    const ticket = reqGetOrdenDB[0];
+    console.log(ticket);
+    const sessionDetails = await getStripeSessionDetails(ticket?.id_session);
+    //console.log(sessionDetails);
+    if (ticket?.id && ticket?.id_ticket === 'none' && sessionDetails.payment_status === 'unpaid') {
+        console.log('El pago se ha concretado con exito. Actualizando el estatus en la API de tickets...');
+        console.log('https://api.doctorsim.com/create_order/{id_terminal}/{id_operador}/{imei}/{id_servicio}')
+        const urlCreateTicket = `${apiDrSimCreateOrden}/${ticket.id_terminal}/${ticket.id_operador}/${ticket.imei}/${ticket.id_service}`;
+        console.log(urlCreateTicket)
+        const reqDrSimCreateOrden = await sendPostRequestDRSIM(urlCreateTicket);
+        //const resDrSimCreateOrden = JSON.parse(reqDrSimCreateOrden);
+        console.log(reqDrSimCreateOrden);
+        var nroTicket = 'none';
+        if (reqDrSimCreateOrden?.res?.id_ticket){
+            nroTicket = reqDrSimCreateOrden.res.id_ticket;
+            console.log(`ticket creado: ${nroTicket}`);
+        }
+        /*
+        const reqTicket = {
+            id: ticket.id,
+            id_ticket: nroTicket,
+            date: ticket.date,
+            email: ticket.email,
+            imei: ticket.imei,
+            id_payment: ticket.id_payment,
+            price: ticket.price,
+            estatus: sessionDetails.payment_status,
+            id_session: ticket.id_session,
+            dataComplete: ticket.dataComplete + `, ticket: ${nroTicket}`,
+            id_terminal: ticket.id_terminal,
+            id_operador: ticket.id_operador,
+        };
+
+        const optionsPutDynamo = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+        urlOrden = `https://0v8aexvf86.execute-api.us-east-1.amazonaws.com/tickets/ordenes`;
+        const reqDynamoCreateOrden = await sendToDynamoBD(urlOrden, optionsPutDynamo, reqTicket);
+        console.log(reqDynamoCreateOrden);
+        */
+        body = { message: 'Pago validado con éxito. Estatus actualizado a "paid".' };
+    } else {
+        console.log('Esta condicion no se cumplio: if (ticket.id && ticket.id_ticket === none && sessionDetails.payment_status === paid)');
+        body = { message: 'El pago no se ha concretado con éxito.' };
+    }
+
+}
+
+const getStripeSessionDetails = async (sessionId) => {
+    const apiUrl = `https://api.stripe.com/v1/checkout/sessions/${sessionId}`;
+    try {
+      const response = await axios.get(apiUrl, {
+        headers: {
+            Authorization: `Bearer ${process.env.STRIPESECRETKEY}`,
+        },
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error('getStripeSessionDetails error:', error);
+      return null;
+    }
+};
+
+async function createCheckoutSession(connection, postRequest){
     const urlTool = `https://api.doctorsim.com/tools/${postRequest.id_terminal}/${postRequest.id_operador}`;
     try {
         const responseTools = await sendHttpRequestDRSIM(urlTool);
         const tools = responseTools.res.tools;
         var tool;
+        var body;
         for await (const item of tools) {
             if (item.id_tool === postRequest.id_service) {
                 tool = item;
                 break;
             }
-        }
-        return tool;
+        }if (tool) {
+            const timestamp = Date.now();
+            const producto = await stripe.products.create({
+                name: tool.name,
+                description: tool.service_name,
+            });
+
+            const precio = await stripe.prices.create({
+                product: producto.id,
+                unit_amount: parseInt(tool.price * 100),
+                currency: 'USD',
+            });
+            
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        price: precio.id,
+                        quantity: 1,
+                    },
+                ],
+                mode: 'payment',
+                success_url: `${postRequest.urlDomain}/${timestamp}`,
+                cancel_url: `${postRequest.urlDomain}/cancel`,
+            });
+
+            if (session.id) {                
+                const hoy = await formatDateString(timestamp);
+                const reqTicket = {
+                    id: timestamp,
+                    id_ticket: 'none',
+                    date: hoy,
+                    email: postRequest.email,
+                    imei: postRequest.imei,
+                    id_service: postRequest.id_service,
+                    price: `${tool.price}`,
+                    estatus: session.payment_status,
+                    id_session: session.id,
+                    dataComplete: `IMEI: ${postRequest.imei}, idService: ${postRequest.id_service}, email: ${postRequest.email}, price: ${tool.price}, date: ${hoy}`,
+                    id_terminal: postRequest.id_terminal,
+                    id_operador: postRequest.id_operador,
+                };
+                
+                const reqCreateOrdenDB = await saveData(connection, reqTicket, 'ordenes');
+            } else {
+                body = { sessionId: `${urlDomain}/cancel` };
+            }
+            body = { sessionId: session.url };            
+        } 
+
+        return body;
+
     } catch (error) {
         console.log({function: 'createCheckoutSession', error: error});
         return tool={};
     }    
+}
+
+async function formatDateString(dateString) {
+    const date = new Date(dateString); // Crea un objeto Date a partir del string de fecha
+    const year = date.getFullYear(); // Obtiene el año (cuatro dígitos)
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Obtiene el mes (de 0 a 11)
+    const day = date.getDate().toString().padStart(2, '0'); // Obtiene el día del mes
+    const hours = date.getHours().toString().padStart(2, '0'); // Obtiene las horas
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Obtiene los minutos
+    const seconds = date.getSeconds().toString().padStart(2, '0'); // Obtiene los segundos
+
+    // Formatea la fecha en el formato deseado
+    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    return formattedDate;
 }
 
 const sendHttpRequestDRSIM = async (url) => {
@@ -129,6 +271,26 @@ const sendHttpRequestDRSIM = async (url) => {
       return response.data;
     } catch (error) {
       console.error('DRSIM error:', error);
+      return null;
+    }
+};
+
+const sendPostRequestDRSIM = async (url, body) => {
+    console.log({
+                url: url,
+                DSIM_KEY: process.env.DSIM_KEY,
+                DSIM_SECRET: process.env.DRSIM_SECRET,
+            });
+    const headers = {
+        DSIM_KEY: process.env.DSIM_KEY,
+        DSIM_SECRET: process.env.DRSIM_SECRET
+    };        
+    try {
+        const response = await axios.post(url, null, { headers });
+
+        return response.data;
+    } catch (error) {
+      console.error('sendPostRequestDRSI error:', error);
       return null;
     }
 };
