458c8a771a2359eac4c421b230cb009abfff8a9f
James <38962452+wan03@users.noreply.github.com>
2024-02-27 20:19:06 -0500

    Integracion inicial HelcimPay

--- src/functions/httpTriggerBackendDesbloqueamicell.js
+++ src/functions/httpTriggerBackendDesbloqueamicell.js
@@ -12,6 +12,7 @@ const DBHOST = process.env.DBHOST;
 const DBUSER = process.env.DBUSER;
 const DBPASSW = process.env.DBPASSW;
 const DBNAME = process.env.DBNAME;
+const HELCIMAPITOKEN = process.env.HELCIMAPITOKEN
 
 const stripe = Stripe(STRIPESECRETKEY);
 
@@ -20,7 +21,7 @@ app.http('httpTriggerBackendDesbloqueamicell', {
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url: "${request.url}"`);
-        const rutaAlArchivo = './DigiCertGlobalRootCA.crt.pem';        
+        const rutaAlArchivo = './DigiCertGlobalRootCA.crt.pem';
 
         //const name = request.query.get('name') || await request.text() || 'world';
 
@@ -30,9 +31,10 @@ app.http('httpTriggerBackendDesbloqueamicell', {
                 user: DBUSER,
                 password: DBPASSW,
                 database: DBNAME,
-                ssl: {ca: `${rutaAlArchivo}`,
-                        rejectUnauthorized: false,
-                    },
+                ssl: {
+                    ca: `${rutaAlArchivo}`,
+                    rejectUnauthorized: false,
+                },
             });
             const valor = request.query.get('valor');
             const field = request.query.get('field');
@@ -40,69 +42,69 @@ app.http('httpTriggerBackendDesbloqueamicell', {
             const endPoint = request.query.get('endpoint');
             switch (request.method) {
                 case "GET":
-                    if (endPoint && endPoint == 'checkout'){
+                    if (endPoint && endPoint == 'checkout') {
                         const responseCheckout = await validateCheckout(connection, valor);
                         context.res = {
                             body: JSON.stringify(responseCheckout)
                         }
-                    }else{
-                        const responseGet = await getItem(connection, tab, field,  valor);
+                    } else {
+                        const responseGet = await getItem(connection, tab, field, valor);
                         context.res = {
                             body: JSON.stringify(responseGet),
                         };
                     }
                     break;
-                
+
                 case "POST":
                     const requestDataPost = JSON.parse(await request.text());
                     if (requestDataPost) {
-                        if (endPoint && endPoint == 'create-checkout-session'){
+                        if (endPoint && endPoint == 'create-checkout-session') {
                             const createSession = await createCheckoutSession(connection, requestDataPost);
                             console.log(createSession);
-                            context.res = {                            
+                            context.res = {
                                 body: JSON.stringify(createSession),
                                 statusCode: 200,
                             };
                         } else {
                             const resultInsert = await saveData(connection, requestDataPost, tab);
-                            context.res = {                            
-                                body: {id: resultInsert.insertId},
+                            context.res = {
+                                body: { id: resultInsert.insertId },
                                 statusCode: 200,
                             };
-                        }    
+                        }
                     } else {
-                        context.res = {                        
+                        context.res = {
                             body: 'Revise el Cuerpo del Post',
                             statusCode: 500,
                         };
-                    }                    
+                    }
 
                     break;
 
-                    case "PUT":                    
-                        const requestDataPut = JSON.parse(await request.text());
-                        if (requestDataPut && tab) {
-                            const resultUpdate = await updateData(connection, requestDataPut, tab);
-                            context.log(resultUpdate);
-                            context.res = {                            
-                                body: JSON.stringify(resultUpdate),
-                                statusCode: 200,
-                            };
-                        } else {
-                            
-                            if (requestDataPut.migrar === "ALL"){
-                                
-                                const migrados = await saveDataFromAPI(connection);
-                            }
-                            
-                            context.res = {                        
-                                body: 'Completado!',
-                                statusCode: 200,
-                            };
-                        }                    
+                case "PUT":
+                    const requestDataPut = JSON.parse(await request.text());
+                    if (requestDataPut && tab) {
+                        const resultUpdate = await updateData(connection, requestDataPut, tab);
+                        context.log(resultUpdate);
+                        context.res = {
+                            body: JSON.stringify(resultUpdate),
+                            statusCode: 200,
+                        };
+                    } else {
 
-                        break;    
-            
+                        if (requestDataPut.migrar === "ALL") {
+
+                            const migrados = await saveDataFromAPI(connection);
+                        }
+
+                        context.res = {
+                            body: 'Completado!',
+                            statusCode: 200,
+                        };
+                    }
+
+                    break;
+
                 default:
                     context.res = {
                         status: 405,
@@ -118,12 +120,12 @@ app.http('httpTriggerBackendDesbloqueamicell', {
                 body: `Error: ${error.message}`,
             };
             return context.res;
-        }        
+        }
     }
 });
 
-async function validateCheckout(connection, sessionId){     
-    const reqGetOrdenDB = await getItem(connection, 'ordenes', 'id',  sessionId)
+async function validateCheckout(connection, sessionId) {
+    const reqGetOrdenDB = await getItem(connection, 'ordenes', 'id', sessionId)
     const ticket = reqGetOrdenDB[0];
     console.log(ticket);
     const sessionDetails = await getStripeSessionDetails(ticket?.id_session);
@@ -137,7 +139,7 @@ async function validateCheckout(connection, sessionId){
         //const resDrSimCreateOrden = JSON.parse(reqDrSimCreateOrden);
         console.log(reqDrSimCreateOrden);
         var nroTicket = 'none';
-        if (reqDrSimCreateOrden?.res?.id_ticket){
+        if (reqDrSimCreateOrden?.res?.id_ticket) {
             nroTicket = reqDrSimCreateOrden.res.id_ticket;
             console.log(`ticket creado: ${nroTicket}`);
         }
@@ -178,20 +180,20 @@ async function validateCheckout(connection, sessionId){
 const getStripeSessionDetails = async (sessionId) => {
     const apiUrl = `https://api.stripe.com/v1/checkout/sessions/${sessionId}`;
     try {
-      const response = await axios.get(apiUrl, {
-        headers: {
-            Authorization: `Bearer ${STRIPESECRETKEY}`,
-        },
-      });
+        const response = await axios.get(apiUrl, {
+            headers: {
+                Authorization: `Bearer ${STRIPESECRETKEY}`,
+            },
+        });
 
-      return response.data;
+        return response.data;
     } catch (error) {
-      console.error('getStripeSessionDetails error:', error);
-      return null;
+        console.error('getStripeSessionDetails error:', error);
+        return null;
     }
 };
 
-async function createCheckoutSession(connection, postRequest){
+async function createCheckoutSession(connection, postRequest) {
     const urlTool = `https://api.doctorsim.com/tools/${postRequest.id_terminal}/${postRequest.id_operador}`;
     try {
         const responseTools = await sendHttpRequestDRSIM(urlTool);
@@ -203,32 +205,12 @@ async function createCheckoutSession(connection, postRequest){
                 tool = item;
                 break;
             }
-        }if (tool) {
+        } if (tool) {
             const timestamp = Date.now();
-            const producto = await stripe.products.create({
-                name: tool.name,
-                description: tool.service_name,
-            });
+            const session = checkoutStripe(timestamp)
+            const helcim = createCheckoutHelcim()
 
-            const precio = await stripe.prices.create({
-                product: producto.id,
-                unit_amount: parseInt(tool.price * 100),
-                currency: 'USD',
-            });
-            
-            const session = await stripe.checkout.sessions.create({
-                line_items: [
-                    {
-                        price: precio.id,
-                        quantity: 1,
-                    },
-                ],
-                mode: 'payment',
-                success_url: `${postRequest.urlDomain}/${timestamp}`,
-                cancel_url: `${postRequest.urlDomain}/cancel`,
-            });
-
-            if (session.id) {                
+            if (helcim.checkoutToken) {
                 const hoy = await formatDateString(timestamp);
                 const reqTicket = {
                     idreg: timestamp,
@@ -238,74 +220,95 @@ async function createCheckoutSession(connection, postRequest){
                     imei: postRequest.imei,
                     id_servicio: postRequest.id_service,
                     precio: `${tool.price}`,
-                    estatus: session.payment_status,
-                    id_session: session.id,
+                    estatus: 'pending',
+                    id_session: helcim.secretToken,
                     datos_complementarios: `IMEI: ${postRequest.imei}, idService: ${postRequest.id_service}, email: ${postRequest.email}, price: ${tool.price}, date: ${hoy}`,
                     id_terminal: postRequest.id_terminal,
                     id_operador: postRequest.id_operador,
                 };
-                
+
                 const reqCreateOrdenDB = await saveData(connection, reqTicket, 'ordenes_desbloqueos');
             } else {
                 body = { sessionId: `${urlDomain}/cancel` };
             }
-            body = { sessionId: session.url };            
-        } 
+            body = { sessionId: helcim.checkoutToken };
+        }
 
         return body;
 
     } catch (error) {
-        console.log({function: 'createCheckoutSession', error: error});
-        return tool={};
-    }    
+        console.log({ function: 'createCheckoutSession', error: error });
+        return tool = {};
+    }
+}
+
+const checkoutStripe = async (timestamp) => {
+    const precio = await stripe.prices.create({
+        product: producto.id,
+        unit_amount: parseInt(tool.price * 100),
+        currency: 'USD',
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+                price: precio.id,
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${postRequest.urlDomain}/${timestamp}`,
+        cancel_url: `${postRequest.urlDomain}/cancel`,
+    });
+    return session;
 }
 
 const sendHttpRequestDRSIM = async (url) => {
     try {
-      const response = await axios.get(url, {
-        headers: {
-            DSIM_KEY: DSIM_KEY,
-            DSIM_SECRET: DRSIM_SECRET,
-        },
-      });
+        const response = await axios.get(url, {
+            headers: {
+                DSIM_KEY: DSIM_KEY,
+                DSIM_SECRET: DRSIM_SECRET,
+            },
+        });
 
-      return response.data;
+        return response.data;
     } catch (error) {
-      console.error('DRSIM error:', error);
-      return null;
+        console.error('DRSIM error:', error);
+        return null;
     }
 };
 
 const sendPostRequestDRSIM = async (url, body) => {
     console.log({
-                url: url,
-                DSIM_KEY: DSIM_KEY,
-                DSIM_SECRET: DRSIM_SECRET,
-            });
+        url: url,
+        DSIM_KEY: DSIM_KEY,
+        DSIM_SECRET: DRSIM_SECRET,
+    });
     const headers = {
         DSIM_KEY: DSIM_KEY,
         DSIM_SECRET: DRSIM_SECRET
-    };        
+    };
     try {
         const response = await axios.post(url, null, { headers });
 
         return response.data;
     } catch (error) {
-      console.error('sendPostRequestDRSI error:', error);
-      return null;
+        console.error('sendPostRequestDRSI error:', error);
+        return null;
     }
 };
 
 async function getItem(connection, tableName, field, valor) {
     try {
-        if (valor === undefined || valor === null) {            
+        if (valor === undefined || valor === null) {
             query = `SELECT * FROM ${tableName}`;
             params = [];
-        } else {            
+        } else {
             query = `SELECT * FROM ${tableName} WHERE ${field} = ?`;
             params = [valor];
         }
-      
+
         const results = await connection.query(query, params);
         console.log(results);
         return results;
@@ -314,13 +317,13 @@ async function getItem(connection, tableName, field, valor) {
     }
 }
 
-async function updateData(connection, values, tableName) {    
+async function updateData(connection, values, tableName) {
     const updateCommand = await buildUpdateCommandFromJSON(tableName, values);
     console.log(updateCommand);
     const { id, ...updateValues } = values;  // Excluye el 'id' de los campos a actualizar
     let valuesArray = Object.values(updateValues);
-    valuesArray.push(values.id);    
-     
+    valuesArray.push(values.id);
+
     console.log(valuesArray);
     return new Promise((resolve, reject) => {
         connection.query(updateCommand, valuesArray, (err, results) => {
@@ -354,36 +357,60 @@ async function saveDataFromAPI(connection) {
             tableName: 'dispositivos_celulares',
         }
     ];
-    let migrado = { paises: [], operadoras: [], marcas: [], modelos: []};
+    let migrado = { paises: [], operadoras: [], marcas: [], modelos: [] };
     try {
-        
+
         for (const data of dataFromAPI) {
             let porMigrar = [];
-            if (data.tableName === 'paises'){
-                const responsePaises = await sendHttpRequestDRSIM(data.url); 
+            if (data.tableName === 'paises') {
+                const responsePaises = await sendHttpRequestDRSIM(data.url);
                 porMigrar = await saveDataFromApiPaises(connection, data, responsePaises);
                 migrado.paises = porMigrar;
             }
-            if (data.tableName === 'operadoras'){
-                const responseOperadoras = await sendHttpRequestDRSIM(data.url); 
+            if (data.tableName === 'operadoras') {
+                const responseOperadoras = await sendHttpRequestDRSIM(data.url);
                 porMigrar = await saveDataFromApiOperadoras(connection, data, responseOperadoras);
                 migrado.operadoras = porMigrar;
             }
-            if (data.tableName === 'marcas_celulares'){
-                const responseMarcas = await sendHttpRequestDRSIM(data.url); 
+            if (data.tableName === 'marcas_celulares') {
+                const responseMarcas = await sendHttpRequestDRSIM(data.url);
                 porMigrar = await saveDataFromApiMarcas(connection, data, responseMarcas);
                 migrado.marcas = porMigrar;
             }
-            if (data.tableName === 'dispositivos_celulares'){
-                const responseModelos = await sendHttpRequestDRSIM(data.url); 
+            if (data.tableName === 'dispositivos_celulares') {
+                const responseModelos = await sendHttpRequestDRSIM(data.url);
                 porMigrar = await saveDataFromApiModelos(connection, data, responseModelos);
                 migrado.modelos = porMigrar;
             }
-            
-        }              
+
+        }
         return migrado;
-        
+
     } catch (error) {
         console.error('Error en la funcion saveDataFromAPI:', error);
     }
+}
+
+const createCheckoutHelcim = async precio => {
+    const payload = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'api-token': HELCIMAPITOKEN,
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({ paymentType: 'purchase', amount: precio, currency: 'USD' })
+    }
+
+    axios.post('https://api.helcim.com/v2/helcim-pay/initialize', payload)
+        .then(response => {
+            console.log(response)
+            return response
+        }
+        )
+        .catch(err => {
+            console.error(err)
+            return err
+        });
+
 }
